7df6b8e0ab546e68ffdaffe2c9bda726351a572b
yonghangtian <yonghangtian@gmail.com>
2019-06-12 23:35:59 +0800

    add enum difficulty

---
+++ src/main/java/com/example/recipe/domain/Difficulty.java
@@ -0,0 +1,9 @@
+package com.example.recipe.domain;
+
+/**
+ * @author tianyh
+ * created by tianyh on 6/12/19 11:01 PM
+ */
+public enum  Difficulty {
+    EASY, MODERATE, KIND_OF_HARD, HARD
+}
--- src/main/java/com/example/recipe/domain/Recipe.java
+++ src/main/java/com/example/recipe/domain/Recipe.java
@@ -21,8 +21,10 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    //private Difficulty difficulty;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
 
