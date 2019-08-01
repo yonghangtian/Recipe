5dc59cb699003c09a4643ea4c183b1e63260b6b1
yonghangtian <yonghangtian@gmail.com>
2019-08-01 17:06:41 +0800

    refactor to persist images to datebase.

--- src/main/java/com/example/recipe/service/ImageServiceImpl.java
+++ src/main/java/com/example/recipe/service/ImageServiceImpl.java
@@ -1,15 +1,52 @@
 package com.example.recipe.service;
 
+import com.example.recipe.domain.Recipe;
+import com.example.recipe.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Optional;
 
 /**
  * created by tianyh on 7/30/19 3:49 PM
  */
+@Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
+
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
-    public void saveImageFile(Long valueOf, MultipartFile file) {
-        return;
+    @Transactional
+    public void saveImageFile(Long recipeId, MultipartFile file) {
+        try{
+            Optional<Recipe> recipeOptional = recipeRepository.findById(recipeId);
+            if (recipeOptional.isPresent()) {
+                Recipe recipe = recipeOptional.get();
+
+                Byte[] byteObject = new Byte[file.getBytes().length];
+
+                int i = 0;
+
+                for (byte b : file.getBytes()) {
+                    byteObject[i++] = b;
+                }
+                recipe.setImage(byteObject);
+
+                recipeRepository.save(recipe);
+            } else {
+                log.error("No Recipe found for id: " + recipeId );
+            }
+        } catch (IOException e) {
+            log.error("MultipartFile error occurred.");
+            e.printStackTrace();
+        }
     }
 }
--- src/test/java/com/example/recipe/service/ImageServiceImplTest.java
+++ src/test/java/com/example/recipe/service/ImageServiceImplTest.java
@@ -1,10 +1,60 @@
 package com.example.recipe.service;
 
+import com.example.recipe.domain.Recipe;
+import com.example.recipe.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.awt.*;
+import java.util.Optional;
+
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * created by tianyh on 7/30/19 3:49 PM
  */
 public class ImageServiceImplTest {
 
+    @Mock
+    RecipeRepository recipeRepository;
+
+    ImageService imageService;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        imageService = new ImageServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void testSaveImageFile() throws Exception {
+        //given
+        Long id = 1L;
+        MultipartFile multipartFile = new MockMultipartFile("imagefile", "testing.txt", "text/plain",
+                "Spring framework Guru".getBytes());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(id);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        ArgumentCaptor<Recipe> argumentCaptor = ArgumentCaptor.forClass(Recipe.class);
+        //when
+        imageService.saveImageFile(id,multipartFile);
+
+        //then
+        verify(recipeRepository).save(argumentCaptor.capture());
+        Recipe savedRecipe = argumentCaptor.getValue();
+        assertEquals(multipartFile.getBytes().length, savedRecipe.getImage().length);
+    }
 }
